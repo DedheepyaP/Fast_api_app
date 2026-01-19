5208f4d922c19b59b7afaf5d7e35cccb8be790b7
Dedheepya P <dedheepya@Dedheepyas-MacBook-Pro.local>
2026-01-19 16:55:40 +0530

    change

--- backend/models.py
+++ backend/models.py
@@ -5,11 +5,3 @@ class products(BaseModel):
     description : str
     price :float
     quantity: int
-
-    # def __init__(self, id: int, name: str, description: str, price: float, quantity: int):
-    #     self.id= id
-    #     self.name= name
-    #     self.description= description
-    #     self.price= price
-    #     self.quantity=quantity
-        
